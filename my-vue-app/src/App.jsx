7e967d3dbdd2739f2b1752eb4d6dac9e79c11ca4
code-with-smart <smartfaiza53@gmail.com>
2023-12-30 04:12:22 +0500

    Assignment-4

---
+++ my-vue-app/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Navbar from './assets/All.coponents/Navbar'
+import BgImg from './assets/All.coponents/Bgimg'
+import Compoonent_1 from './assets/All.coponents/Coponent_1'
+import Component_2 from './assets/All.coponents/Coponent_2'
+import Component_3 from './assets/All.coponents/Coponent_3'
+import Component_4 from './assets/All.coponents/Coponent_4'
+import Component_5 from './assets/All.coponents/Coponent_5'
+import Component_6 from './assets/All.coponents/Coponent_6'
+import Component_7 from './assets/All.coponents/Coponent_7'
+function App() {
+  return (
+   <>
+   <Navbar/>
+   <BgImg/>
+   < Compoonent_1/>
+   <Component_2/>
+   <Component_3/>
+   <Component_4/>
+   <Component_5/>
+   <Component_6/>
+   <Component_7/>
+   
+   
+
+   
+  
+
+
+  
+
+   </>
+  )
+}
+
+export default App
---
+++ my-vue-app/src/assets/All.coponents/Coponent_7.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function Component07() {
+  return (
+    
+    <>
+    
+<div className='flex'>
+  <div className='h-40  w-2/4 py-5'>
+  
+    <ul className='capitalize flex justify-center gap-x-5	text-colo text-blue-500 underline 	'>
+      <li>about</li>
+      <li>contact</li>
+      <li>terms of use</li>
+      <li>privacy policy</li>
+    </ul>
+    <p className='capitalize flex justify-center '>your website 2023.all right reserved</p>
+
+  </div>
+
+  <div className=' w-2/4 	'>
+    <img className='pl-52 pt-16	' src="images/icons.jpg" alt="" />
+ 
+  </div>
+ </div>
+
+    
+    </>
+  )
+}
+
+export default Component07
